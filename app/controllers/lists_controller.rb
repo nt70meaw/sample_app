b82fcf728db2a9ce808039189e8aad4cef8d1e95
EC2 Default User <ec2-user@ip-172-31-43-110.eu-north-1.compute.internal>
2025-02-25 16:07:13 +0000

    sample_app

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,47 +1,47 @@
 class ListsController < ApplicationController
 
   def new
-    @list = List.new
+   @list = List.new
   end
 
-    def create
-      @list = List.new(list_params)
-      if @list.save
-         flash[:notice] = "投稿に成功しました。"
-        redirect_to list_path(@list.id)
-      else
-        flash.now[:alert] = "投稿に失敗しました。"
-        render :new
-      end
+  def create
+   @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to list_path(list.id)
+    else
+      flash.now[:alert] = "投稿に失敗しました。"
+      render :new
+    end
   end
   
   def show
-    @list = List.find(params[:id])  
+   @list = List.find(params[:id])  
   end
 
   def index
-  @lists = List.all
+   @lists = List.all
   end
 
   def edit
-    @list = List.find(params[:id])
+   @list = List.find(params[:id])
   end
 
   def update
-    list = List.find(params[:id])
-    list.update(list_params)
-    redirect_to list_path(list.id)  
+   list = List.find(params[:id])
+   list.update(list_params)
+   redirect_to list_path(list.id)  
   end
  
   def destroy
-    list = List.find(params[:id])
-    list.destroy 
-    redirect_to '/lists' 
+   list = List.find(params[:id])
+   list.destroy 
+   redirect_to '/lists' 
   end
 
-  private
+private
   def list_params
-    params.require(:list).permit(:title, :body, :image)
+   params.require(:list).permit(:title, :body, :image)
   end
 end
 
